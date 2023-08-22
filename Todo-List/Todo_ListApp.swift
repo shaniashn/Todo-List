fddeb1bb1bb10a950519dbe2afb1b8e308023a3b
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-08-23 02:50:38 +0700

    Initial Commit

---
+++ Todo-List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo-List
+//
+//  Created by Sha Nia Siahaan on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
