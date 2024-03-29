ceb48c40a853afff9e17c91a8a206db0dc71fc33
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-03-29 23:41:20 +0700

    Update TodoViewModel.swift

--- Todo-List/ViewModels/TodoViewModel.swift
+++ Todo-List/ViewModels/TodoViewModel.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+//class taskManager: ObservableObject {
+//    @Published var cTask: Int = 0
+//}
+
 class TodoViewModel: ObservableObject {
     @Published var todoModel = TodoModel()
+    @Published var countTask: Int = 0
     
     var todos: [Todo] {
         return todoModel.todos
@@ -16,6 +21,11 @@ class TodoViewModel: ObservableObject {
     
     var tasks: [Tasks] {
         return todoModel.tasks.reversed()
+    }
+    
+    init() {
+        countTheTask()
+        
     }
     
     func addTodo(todo: Todo) {
@@ -32,23 +42,27 @@ class TodoViewModel: ObservableObject {
     
     func updatedTask(theTask: Tasks) { //fungsi ini panggil fungsi yang dari todoModel
         todoModel.updateTask(theTask: theTask)
+        //MARK: fungsi 
+//        countTheTask()
+//        print("dalam fungsi updatedtask", countTask)
+        
+//        countTheTasks(pos: theTask)
+        
     }
     
     func moveItem(from: IndexSet, to: Int) {
         todoModel.moveItem(from: from, to: to)
     }
     
+    func countTheTask(){
+        countTask = tasks.filter { $0.isCompleted == true }.count
+        print("dalam fungsi count, muncul karena di dalam fungsi countTheTask() yg di .init() TodoViewModel", countTask)
+    }
     
+    func countTheTasks(pos: Tasks){
+//        todoModel.countCompleteTask(id: todos[pos].tasks)
+//        todoModel.countCompleteTaskk(taks: tasks[pos])
+//        countTask = todoModel.countCompleteTaskk(taks: pos)
+    }
     
-//    func showData() -> Todo {
-//        do {
-//            var todo = try todos
-//            return todo
-//        }
-//        if todos.isEmpty {
-//            return
-//        } else {
-//            return todos
-//        }
-//    }
 }
